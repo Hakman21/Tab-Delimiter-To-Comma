7c4041fcd82392594a0f563f84ba5ad86b9d657a
Hakman21 <90862107+Hakman21@users.noreply.github.com>
2023-08-18 19:40:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-open("data.txt", "a").close()
-
-def replace():
-
-    with open("data.txt") as fin, open("newdatafile.txt", "w") as fout:
+with open("data.txt") as fin, open("newdatafile.txt", "w") as fout:
         for line in fin:
             fout.write(line.replace('\t', ','))
-
-hello = True
-while hello == True:
-            
-    start = input("Start? y/n: ")
-    
-    if start == "y":
-        hola = replace()
-        hello = False
-    elif start == "n":
-        hello = False
-    else:
-        print(f"Invalid Input, please try again. ")
